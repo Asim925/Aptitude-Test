3b3a8793bc07012e411d9c15c278558f2c1edf31
Asim Nazeer <asimnazeer925@gmail.com>
2025-08-29 01:04:01 +0500

    ADDING WALLPAPERS

--- app/dow/page.tsx
+++ app/dow/page.tsx
@@ -4,9 +4,9 @@ import { dowInfo } from "../constants/constants";
 
 const Page = () => {
   return (
-    <div className="sm:my-10 xl:mx-20 sm:mx-15 m-5">
+    <div className="bg-[url('/main-bg.png')] bg-fixed bg-no-repeat sm:py-10 xl:px-20 sm:px-15 p-5">
       <main className="sm:text-center">
-        <h1 className="text-3xl sm:text-5xl font-extrabold">
+        <h1 className="text-amber-400 text-3xl sm:text-5xl font-extrabold">
           DOW University - Admission Test
         </h1>
         <p className="text-md sm:text-lg font-semibold text-gray-100 mt-4 px-2 sm:mt-8">
@@ -16,8 +16,8 @@ const Page = () => {
         </p>
       </main>
       <div className="mt-5">
-        <div className="border rounded-2xl bg-gray-800/70 border-gray-600 p-6">
-          <h2 className="text-2xl sm:text-3xl font-bold mb-4">
+        <div className="border rounded-2xl backdrop-blur-sm bg-orange-800/10 border-amber-600 p-6">
+          <h2 className="text-amber-400 text-2xl sm:text-3xl font-bold mb-4">
             Test Information
           </h2>
           <ul className="list-disc list-inside text-gray-200 space-y-2 text-md sm:text-lg">
@@ -43,18 +43,22 @@ const Page = () => {
 
       {/* Section Breakdown */}
       <div className="mt-8">
-        <h2 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
+        <h2 className="text-amber-400  text-2xl sm:text-3xl font-bold mb-4 text-center">
           Section-wise Breakdown
         </h2>
         <div className="overflow-x-auto">
-          <table className="w-full border-collapse text-gray-100 text-sm sm:text-lg">
+          <table className="w-full backdrop-blur-sm bg-black/50 border-collapse text-gray-100 text-sm sm:text-lg">
             <thead>
-              <tr className="bg-gray-800/70">
-                <th className="border border-gray-600 px-3 py-2">Section</th>
-                <th className="border border-gray-600 px-3 py-2">Questions</th>
-                <th className="border border-gray-600 px-3 py-2">Time</th>
-                <th className="border border-gray-600 px-3 py-2">Weightage</th>
-                <th className="border border-gray-600 px-3 py-2">Negative</th>
+              <tr className="bg-amber-800/70">
+                <th className="border border-orange-600 px-3 py-2">Section</th>
+                <th className="border border-orange-600 px-3 py-2">
+                  Questions
+                </th>
+                <th className="border border-orange-600 px-3 py-2">Time</th>
+                <th className="border border-orange-600 px-3 py-2">
+                  Weightage
+                </th>
+                <th className="border border-orange-600 px-3 py-2">Negative</th>
               </tr>
             </thead>
             <tbody>
@@ -62,22 +66,22 @@ const Page = () => {
                 <tr
                   key={index}
                   className={`${
-                    index % 2 === 0 ? "bg-gray-700/30" : "bg-gray-800/30"
-                  } hover:bg-gray-700/60 transition`}
+                    index % 2 === 0 ? "bg-orange-400/10" : "bg-orange-300/10"
+                  } hover:bg-orange-500/15 transition`}
                 >
-                  <td className="border border-gray-600 px-3 py-2">
+                  <td className="border border-orange-400 px-3 py-2">
                     {sec.name}
                   </td>
-                  <td className="border border-gray-600 px-3 py-2">
+                  <td className="border border-orange-400 px-3 py-2">
                     {sec.questions}
                   </td>
-                  <td className="border border-gray-600 px-3 py-2">
+                  <td className="border border-orange-400 px-3 py-2">
                     {sec.time}
                   </td>
-                  <td className="border border-gray-600 px-3 py-2">
+                  <td className="border border-orange-400 px-3 py-2">
                     {sec.weightage}
                   </td>
-                  <td className="border border-gray-600 px-3 py-2">
+                  <td className="border border-orange-400 px-3 py-2">
                     {sec.negative}
                   </td>
                 </tr>
@@ -91,11 +95,11 @@ const Page = () => {
       <div className="flex justify-center">
         <Link
           href="/dow/start-test"
-          className="px-6 py-4 mt-6 bg-gradient-to-r from-red-700 to-purple-800
+          className="px-6 py-4 mt-6 bg-gradient-to-r from-orange-400 to-amber-900
                      text-white text-lg font-bold rounded-xl shadow
-                     hover:from-red-800 hover:to-pink-700 transition-colors"
+                     hover:from-orange-500 hover:to-amber-900 transition-colors"
         >
-          🚀 Begin My Admission Test
+          Up for the Challenge 🚀?
         </Link>
       </div>
     </div>
