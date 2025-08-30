6526a0178a10a1340ea91e44d09ca9c37d664687
Asim Nazeer <asimnazeer925@gmail.com>
2025-08-31 00:51:00 +0500

    logo, question styling

--- app/api/questions/[subject]/route.ts
+++ app/api/questions/[subject]/route.ts
@@ -2,13 +2,16 @@ import { questions } from "@/app/constants/questions";
 import { NextResponse } from "next/server";
 
 interface Props {
-  params: { subject: string };
+  params: Promise<{ subject: string }>;
 }
 
 export async function GET(request: Request, { params }: Props) {
-  const subjectData = questions.find((q) => q.subject === params.subject);
+  const { subject } = await params;
+  const subjectData = questions.find((q) => q.subject === subject);
+
   if (!subjectData) {
     return NextResponse.json({ error: "Subject not found" }, { status: 404 });
   }
+
   return NextResponse.json(subjectData);
 }
