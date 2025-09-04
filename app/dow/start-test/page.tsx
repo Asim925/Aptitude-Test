a7022b2298b5c9a624cb99dbfea575fd03dc98db
Asim Nazeer <asimnazeer925@gmail.com>
2025-09-05 00:01:01 +0500

    AI Integrated.

--- app/dow/start-test/page.tsx
+++ app/dow/start-test/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import { useState } from "react";
 import Questions from "./test-components/Questions";
 
-const page = () => {
-  let [marks, setMarks] = useState(0);
+const Page = () => {
+  const [marks, setMarks] = useState(0);
+
   return <Questions marks={marks} setMarks={setMarks} />;
 };
 
-export default page;
+export default Page;
